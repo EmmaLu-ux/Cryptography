3ce30766ff96401abca47ad2dd7c3b3f5a129b9b
EmmaLu-ux <70001362+EmmaLu-ux@users.noreply.github.com>
2021-06-06 11:49:05 +0800

    Update CaesarEnc.py

--- com/genhua/cryptography/CaesarEnc.py
+++ com/genhua/cryptography/CaesarEnc.py
@@ -1,6 +1,8 @@
 import string, sys
 
 def encrypt(k, m):
+    # upper(): 将字符串中的小写字母转为大写字母。
+    # ord(): 返回对应的 ASCII 数值，或者 Unicode 数值。一般与chr()配对使用
     shift = ord(k.upper()) - 65
     c = ""
     for c1 in (m.replace(' ',"")).upper():
@@ -13,6 +15,7 @@ def encrypt(k, m):
 
 if __name__ == '__main__':
     try:
+        # 从用户的输入中获取参数
         k = sys.argv[1]
         m = sys.argv[2]
     except:
@@ -20,25 +23,4 @@ if __name__ == '__main__':
         m = input("Plaintext:")
         c = encrypt(k, m)
         print(' '.join(c[i:i+5] for i in range(0, len(c), 5)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
